14a025f72000cc67b77862ec6555227acae06860
James Geneser <jamesgeneser@gmail.com>
2023-02-25 10:41:50 -0700

    fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,29 @@ var apiURL = "https://www.googleapis.com/books/v1/volumes?q=mobydick+inauthors:m
 
 
 
-fetch(apiURL)
-.then(function(response){
-    //console.log(response)
-    return response.json()
+// fetch(apiURL)
+// .then(function(response){
+//     //console.log(response)
+//     return response.json()
 
-})
-.then(function(data){
-    console.log(data)
-})
+// })
+// .then(function(data){
+//     console.log(data)
+// })
+
+var apiURL =
+  "https://www.googleapis.com/books/v1/volumes?q=universeinanutshell&key=YOURAPIKEY";
+
+fetch(apiURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.items);
+    var testArr = data.items;
+    for (var i = 0; i < testArr.length; i++) {
+      console.log(testArr[i].volumeInfo.authors);
+      console.log(testArr[i].volumeInfo.title);
+      console.log(testArr[i].volumeInfo.description);
+    }
+  });
