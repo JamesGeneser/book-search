25ba38851847763d77cabd01977d0213e7702b11
James Geneser <jamesgeneser@gmail.com>
2023-02-26 08:52:41 -0700

    working data from volume search API

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,62 +10,95 @@ var apiURL =
   "&key=" +
   apiKey;
 
-fetch(apiURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.items);
-    var searchArr = data.items;
-    for (var i = 0; i < searchArr.length; i++) {
+function getApiSearchResults(){
+    fetch(apiURL)
+    .then(function (response) {
+     return response.json();
+     })
+         .then(function (data) {
+        console.log(data.items);
+        var searchArr = data.items;
+        for (var i = 0; i < searchArr.length; i++) {
         
         var titleResults = searchArr[i].volumeInfo.title;
 
-        var titleId = searchArr[i].id
+        titleId = searchArr[i].id
         console.log("volumeinfo.title"+titleResults)
         console.log(titleId)
-        $("#titleResults").append("<a href='' id="+titleId+" >" + titleResults + "</a>"+ "<br>")
-        
+        $("#titleResults").append("<a href='' class='links' id="+titleId+" >" + titleResults + "</a>"+ "<br>").click(getIdApi)
+        }
+    }
+    )
+}
+getApiSearchResults()  
 
-        var authorResults = searchArr[i].volumeInfo.authors
-        // console.log("volumeinfo.authors"+authorResults);
-        $("#authorResults").append("<li>" + authorResults + "</li>")
+function getIdApi(event){
+        event.preventDefault()
+        var idNum = event.target.id
+        console.log(idNum)
 
-    
+        var titleSearchApiUrl = "https://www.googleapis.com/books/v1/volumes/"+
+        idNum +
+        "?key="+
+        apiKey;
+       
+        fetch(titleSearchApiUrl)
+        .then(function (response) {
+            console.log(response.status)
+          return response.json();
+          
+        })
+        .then(function (data) {
+          console.log(data);
+          var bookTitle
+          var bookSubTitle
+          var bookAuthor
+          var bookPublishDate
+          var bookPublisher
+          var bookPageCount
+
+})
+}
+
+        // var authorResults = searchArr[i].volumeInfo.authors
+        // // console.log("volumeinfo.authors"+authorResults);
+        // $("#authorResults").append("<li>" + authorResults + "</li>")
 
     }
-})
+)
 
 
-        var linkEl = $("#titleResults")
-        console.log(linkEl)
-        linkEl.on("click",showBookData)
-
-
-
-  function showBookData(event){
-    event.preventDefault()
-    selectedTitle = event.target.textContent
-    console.log(selectedTitle)
-    //Specific book search 
-        var bookApiURL = "https://www.googleapis.com/books/v1/volumes?q="+
-        selectedTitle +
-        "&intitle" +
-        "&key=" +
-        apiKey;
-        fetch(bookApiURL)
-         .then(function (response) {
-            return response.json();
-         })
-        .then(function (data) {
-            console.log(data.items);
-            
-        })
         
-  }
+
+
+
+//   function showBookData(event){
+//     event.preventDefault()
+//     var idNum = 
+//     console.log(idNum)
+//}
+
+    // selectedTitle = event.target.textContent
+    // var idNum = selectedTitle.attr("id")
+    // console.log(idNum)
+    //Specific book search 
+        // var bookApiURL = "https://www.googleapis.com/books/v1/volumes/"+
+        //  +
+       
+        // "&key=" +
+        // apiKey;
+        // fetch(bookApiURL)
+        //  .then(function (response) {
+        //     return response.json();
+        //  })
+        // .then(function (data) {
+        //     console.log(data.items);
+            
+        // })
+        
+  
   ////Book Results
 
 //   var eTag = searchArr[j].etag
 // var bookTitle = array 
 // var authorName =
-})
