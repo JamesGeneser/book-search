780add60bbd5f467b10a7426ef2cd1a6d2b3e4fe
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-02-26 13:14:39 -0700

    changed the layout of the index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,67 @@
-
 $(document).ready(function () {
+  var searchInput = "universeinanutshell";
+  var apiKey = "AIzaSyDyUh9tTZjRYDn1uNQbyK8fgrSAGsMKnW4";
+  var apiURL =
+    "https://www.googleapis.com/books/v1/volumes?q=" +
+    searchInput +
+    "&key=" +
+    apiKey;
 
-
-var searchInput = "universeinanutshell"
-var apiKey = "AIzaSyDyUh9tTZjRYDn1uNQbyK8fgrSAGsMKnW4"
-var apiURL =
-  "https://www.googleapis.com/books/v1/volumes?q="+
-  searchInput +
-  "&key=" +
-  apiKey;
-
-fetch(apiURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.items);
-    var searchArr = data.items;
-    for (var i = 0; i < searchArr.length; i++) {
-        
+  fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.items);
+      var searchArr = data.items;
+      for (var i = 0; i < searchArr.length; i++) {
         var titleResults = searchArr[i].volumeInfo.title;
 
-        var titleId = searchArr[i].id
-        console.log("volumeinfo.title"+titleResults)
-        console.log(titleId)
-        $("#titleResults").append("<a href='' id="+titleId+" >" + titleResults + "</a>"+ "<br>")
-        
+        var titleId = searchArr[i].id;
+        console.log("volumeinfo.title" + titleResults);
+        console.log(titleId);
+        $("#titleResults").append(
+          "<a href='' id=" + titleId + " >" + titleResults + "</a>" + "<br>"
+        );
 
-        var authorResults = searchArr[i].volumeInfo.authors
+        var authorResults = searchArr[i].volumeInfo.authors;
         // console.log("volumeinfo.authors"+authorResults);
-        $("#authorResults").append("<li>" + authorResults + "</li>")
+        $("#authorResults").append("<li>" + authorResults + "</li>");
+      }
+    });
 
-    
+  var linkEl = $("#titleResults");
+  console.log(linkEl);
+  linkEl.on("click", showBookData);
 
-    }
-})
-
-
-        var linkEl = $("#titleResults")
-        console.log(linkEl)
-        linkEl.on("click",showBookData)
-
-
-
-  function showBookData(event){
-    event.preventDefault()
-    selectedTitle = event.target.textContent
-    console.log(selectedTitle)
-    //Specific book search 
-        var bookApiURL = "https://www.googleapis.com/books/v1/volumes?q="+
-        selectedTitle +
-        "&intitle" +
-        "&key=" +
-        apiKey;
-        fetch(bookApiURL)
-         .then(function (response) {
-            return response.json();
-         })
-        .then(function (data) {
-            console.log(data.items);
-            
-        })
-        
+  function showBookData(event) {
+    event.preventDefault();
+    selectedTitle = event.target.textContent;
+    console.log(selectedTitle);
+    //Specific book search
+    var bookApiURL =
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      selectedTitle +
+      "&intitle" +
+      "&key=" +
+      apiKey;
+    fetch(bookApiURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data.items);
+      });
   }
   ////Book Results
 
-//   var eTag = searchArr[j].etag
-// var bookTitle = array 
-// var authorName =
-})
+  //   var eTag = searchArr[j].etag
+  // var bookTitle = array
+  // var authorName =
+});
+
+// Author results functions
+
+// function authorApi() {
+//   var author =
+// }
