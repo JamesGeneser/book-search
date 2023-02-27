2dbba046824d4c4689c3b0edc6ac3a2eb55f1eb3
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-02-27 12:40:19 -0700

    changed for loop in indexscript.js

--- assets/js/indexscript.js
+++ assets/js/indexscript.js
@@ -1,53 +1,43 @@
 var searchBtn = $("#searchbutton");
-var searchHistory = []
-var search = ""
+var searchHistory = [];
+var search = "";
 
+function getUserInput(event) {
+  event.preventDefault();
+  searchInput = $("#searchinput").val();
+  localStorage.setItem("userSearch", searchInput);
 
-    function getUserInput(event){
-        event.preventDefault()
-        searchInput = $("#searchinput").val()
-        localStorage.setItem("userSearch",searchInput)
+  searchHistory.push(searchInput);
 
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
-        searchHistory.push(searchInput)
+  location.href = "bookpage.html";
 
-        localStorage.setItem("searchHistory", JSON.stringify(searchHistory))
-    
-        location.href = "bookpage.html"
+  console.log(searchInput);
+}
 
-        console.log(searchInput)
+function createSearchHistoryButton(search) {
+  var savedSearches = $("#bookShelf");
 
+  savedSearches.append("<button>" + search + "</button>");
+}
 
-
-        
-        
+function renderUserSearch() {
+  var userSearchHistory = localStorage.getItem("searchHistory");
+  if (userSearchHistory) {
+    searchHistory = JSON.parse(userSearchHistory);
+    for (var i = 0; i < searchHistory.length; i++) {
+      search = searchHistory[i];
+      console.log(search);
+      createSearchHistoryButton(search);
     }
+  }
+}
 
-    function createSearchHistoryButton(search){
-    
-        var savedSearches = $("#bookShelf")
-
-    
-    savedSearches.append("<button>"+search+"</button>")
-        }
-    
-   
-
-    function renderUserSearch(){
-       
-        var userSearchHistory = (localStorage.getItem("searchHistory"))
-        if (userSearchHistory){
-            searchHistory= JSON.parse(userSearchHistory)
-            for(var i = 0; i<6; i++){
-                search = searchHistory[i]
-                console.log(search)
-                createSearchHistoryButton(search)
-        }}
-    }
-
-
-
-
-//createSearchHistoryButton() 
-renderUserSearch()
-searchBtn.click(getUserInput)
+//   } else if ((userSearchHistory = "")) {
+//     var errorMessage = "No search criteria provided.  Please try again.";
+//     $("searchinput").append(errorMessage);
+//     return errorMessage;
+//createSearchHistoryButton()
+renderUserSearch();
+searchBtn.click(getUserInput);
