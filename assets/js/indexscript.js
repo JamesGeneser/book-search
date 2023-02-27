b130263f550183b9748288dc0a5be6c7acf7bc35
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-02-27 13:33:22 -0700

    updated styling of dynamically populated buttons on index.html and updated back to search button on bookpage.html

--- assets/js/indexscript.js
+++ assets/js/indexscript.js
@@ -19,7 +19,9 @@ function createSearchHistoryButton(search) {
   var savedSearches = $("#bookShelf");
 
   savedSearches.append(
-    "<button class='searchHistoryBtn'>" + search + "</button>"
+    "<button class='searchHistoryBtn waves-effect waves-light btn-small'>" + //don't forget to unedit if this doesnt work.
+      search +
+      "</button>"
   );
 }
 
